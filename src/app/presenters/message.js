ebaba7a85c5ba142c116e02e80fbbfe89d39ffec
DC130 <tonyxu@core-tech.tw>
2022-12-21 18:41:24 +0800

    add

--- src/app/presenters/message.js
+++ src/app/presenters/message.js
@@ -56,45 +56,129 @@ async function setA() {
 
 async function flexMessageResponse(context) {
     return context.replyFlex('123', {
-        type: 'bubble',
-        // hero: {
-        //     type: 'image',
-        //     url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
-        //     size: 'full',
-        //     aspectRatio: '20:13',
-        // },
-        body: {
-            type: 'box',
-            // horizontal / vertical
-            layout: 'horizontal',
-            contents: [
-                {
-                    type: 'text',
-                    text: 'Brown Cafe',
-                    weight: 'bold',
-                    size: 'xl',
+        "type": "carousel",
+        "contents": [
+            {
+                "type": "bubble",
+                "direction": "ltr",
+                "header": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                            "type": "text",
+                            "text": "數字一",
+                            "weight": "bold",
+                            "size": "xxl",
+                            "align": "center",
+                            "gravity": "center",
+                            "contents": []
+                        }
+                    ]
                 },
-                {
-                    type: 'text',
-                    text: 'World!',
+                "hero": {
+                    "type": "image",
+                    "url": "https://eaa1-61-216-67-131.ngrok.io/public/assets/img/game/numbers/0.jpg",
+                    "size": "full",
+                    "aspectRatio": "20:13",
+                    "aspectMode": "fit",
                 },
-            ],
-        },
-
-        footer: {
-            type: 'box',
-            layout: 'vertical',
-            contents: [
-                {
-                    type: 'button',
-                    action: {
-                        type: 'uri',
-                        label: 'WEBSITE',
-                        uri: 'https://linecorp.com',
-                    },
+                "footer": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                            "type": "button",
+                            "action": {
+                                "type": "postback",
+                                "label": "選擇號碼",
+                                "data": "1"
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                "type": "bubble",
+                "direction": "ltr",
+                "header": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                            "type": "text",
+                            "text": "數字二",
+                            "weight": "bold",
+                            "size": "xxl",
+                            "align": "center",
+                            "gravity": "center",
+                            "contents": []
+                        }
+                    ]
                 },
-            ],
-        },
+                "hero": {
+                    "type": "image",
+                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_4_news.png",
+                    "size": "full",
+                    "aspectRatio": "20:13",
+                    "aspectMode": "fit",
+                },
+                "footer": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                            "type": "button",
+                            "action": {
+                                "type": "postback",
+                                "label": "選擇號碼",
+                                "data": "1"
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                "type": "bubble",
+                "direction": "ltr",
+                "header": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                            "type": "text",
+                            "text": "數字三",
+                            "weight": "bold",
+                            "size": "xxl",
+                            "align": "center",
+                            "gravity": "center",
+                            "contents": []
+                        }
+                    ]
+                },
+                "hero": {
+                    "type": "image",
+                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_4_news.png",
+                    "size": "full",
+                    "aspectRatio": "20:13",
+                    "aspectMode": "fit",
+                },
+                "footer": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                            "type": "button",
+                            "action": {
+                                "type": "postback",
+                                "label": "選擇號碼",
+                                "data": "1"
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
     });
 }
 
--- src/resources/views/index.js
+++ src/resources/views/index.js
@@ -1,7 +1,10 @@
 const common = require('./common');
 const presenter = require(common.route.presenter + 'message');
+const fs = require('fs');
 
 module.exports = async function App(context) {
-
-    return presenter.messenger.flexMessageResponse(context);
+    const image = fs.createReadStream('./public/assets/img/game/numbers/0.jpg');
+    return context.sendImage({'type': 'image',
+        'originalContentUrl': image.toString('base64')});
+    // return presenter.messenger.flexMessageResponse(context);
 };
